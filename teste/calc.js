4daf37a0c3e5b32b8ca0fcf761808135d413de6b
Kalister.Dev <43321145+Kalister-dev@users.noreply.github.com>
2020-11-07 22:58:47 -0300

    Atualização monetária

--- teste/calc.js
+++ teste/calc.js
@@ -17,41 +17,46 @@ var total30 = "";
 var valorPerc18 = "";
 var valorPerc30 = "";
 
-
-
 btnAdd.onclick = function add() {
-    dolar = inputDolar.value;
+  dolar = inputDolar.value;
 
-    alert("US$ " + dolar);
+  alert("US$ " + dolar);
 };
 btnItem.onclick = function insertItem() {
-    select = document.getElementById("item");
-    value = select.options[select.selectedIndex].value;
-    alert("US$ " + value);
-
+  select = document.getElementById("item");
+  value = select.options[select.selectedIndex].value;
+  alert("US$ " + value);
 };
 btnReal.onclick = function real() {
-    total = value * dolar;
-    alert("R$ " + total);
-
-
+  total = value * dolar;
+  alert(total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }));
 };
 
 btn18.onclick = function perc18() {
-    valorPerc18 = total * 0.18;
-    total18 = total + valorPerc18;
-    alert("R$ " + total18);
-
-
+  valorPerc18 = total * 0.18;
+  total18 = total + valorPerc18;
+  alert(
+    total18.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+  );
 };
 btn30.onclick = function perc30() {
-    valorPerc30 = total * 0.3;
-    total30 = total + valorPerc30;
-    alert("R$ " + total30);
-
-
+  valorPerc30 = total * 0.3;
+  total30 = total + valorPerc30;
+  alert(
+    total30.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+  );
 };
 btnFinal.onclick = function final() {
-
-    alert("Dolar : US$ " + dolar + "\n\nValor do item : US$ " + value + "\n\nValor em Real : R$ " + total + "\n\nValor com 18 % : US$ " + total18 + "\n\nValor total com 30 % de lucro : US$ " + total30);
+  alert(
+    "Dolar : US$ " +
+      dolar +
+      "\n\nValor do item : US$ " +
+      value +
+      "\n\nValor em Real : " +
+      total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }) +
+      "\n\nValor com 18 % :  " +
+      total18.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }) +
+      "\n\nValor total com 30 % de lucro : " +
+      total30.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+  );
 };
