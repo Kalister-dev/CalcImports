52db23c3698b7742a56cbaa416c7dc8c5086e1d1
Kalister.Dev <43321145+Kalister-dev@users.noreply.github.com>
2020-11-12 18:25:17 -0300

    Atualização

--- calc.js
+++ calc.js
@@ -17,46 +17,41 @@ var total30 = "";
 var valorPerc18 = "";
 var valorPerc30 = "";
 
-btnAdd.onclick = function add() {
-  dolar = inputDolar.value;
 
-  alert("US$ " + dolar);
+
+btnAdd.onclick = function add() {
+    dolar = inputDolar.value;
+
+    alert("US$ " + dolar);
 };
 btnItem.onclick = function insertItem() {
-  select = document.getElementById("item");
-  value = select.options[select.selectedIndex].value;
-  alert("US$ " + value);
+    select = document.getElementById("item");
+    value = select.options[select.selectedIndex].value;
+    alert("US$ " + value);
+
 };
 btnReal.onclick = function real() {
-  total = value * dolar;
-  alert(total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }));
+    total = value * dolar;
+    alert("R$ " + total);
+
+
 };
 
 btn18.onclick = function perc18() {
-  valorPerc18 = total * 0.18;
-  total18 = total + valorPerc18;
-  alert(
-    total18.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-  );
+    valorPerc18 = total * 0.18;
+    total18 = total + valorPerc18;
+    alert("R$ " + total18);
+
+
 };
 btn30.onclick = function perc30() {
-  valorPerc30 = total * 0.3;
-  total30 = total + valorPerc30;
-  alert(
-    total30.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-  );
+    valorPerc30 = total * 0.3;
+    total30 = total + valorPerc30;
+    alert("R$ " + total30);
+
+
 };
 btnFinal.onclick = function final() {
-  alert(
-    "Dolar : US$ " +
-      dolar +
-      "\n\nValor do item : US$ " +
-      value +
-      "\n\nValor em Real : " +
-      total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }) +
-      "\n\nValor com 18 % :  " +
-      total18.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }) +
-      "\n\nValor total com 30 % de lucro : " +
-      total30.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-  );
+
+    alert("Dolar : US$ " + dolar + "\n\nValor do item : US$ " + value + "\n\nValor em Real : R$ " + total + "\n\nValor com 18 % : US$ " + total18 + "\n\nValor total com 30 % de lucro : US$ " + total30);
 };
